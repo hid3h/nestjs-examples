db2397cfa8e010cb45f8bd45dedd547fc53c4a45
hid3h <razyetts@hotmail.co.jp>
2024-10-20 19:59:15 +0900

    タイムゾーン、日時動作確認

--- app-handlebars/src/app.controller.ts
+++ app-handlebars/src/app.controller.ts
@@ -1,4 +1,5 @@
 import { Body, Controller, Get, Post, Render } from '@nestjs/common';
+import { fromZonedTime, toDate } from 'date-fns-tz';
 
 class DateTimeDto {
   datetime: string;
@@ -15,7 +16,24 @@ export class AppController {
   @Post('submit')
   @Render('index')
   postDateTime(@Body() dto: DateTimeDto) {
+    const timezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+    console.log('タイムゾーン:', timezone);
+
+    const now = new Date();
+    console.log('現在日時.new Date()', now);
     const message = `選択された日時: ${dto.datetime}`;
+
+    console.log('リクエストされた日時の文字列.dto.datetime', dto.datetime);
+
+    const requestedDateTime = new Date(dto.datetime);
+    console.log(
+      'リクエストされた日時の文字列をそのままnew Date()',
+      requestedDateTime,
+    );
+
+    const parsedDate = toDate(dto.datetime, { timeZone: 'Asia/Tokyo' });
+    console.log('date-fns-tz/toDate', parsedDate);
+
     return { message };
   }
 }
